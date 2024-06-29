8c35a5b39c151cf7a293901580d3e7e24370f7ae
ANIMIXOM <maximshlyamnev@yandex.ru>
2024-06-29 15:08:26 +0300

    release-game-level1-alpha 0.0.0.1

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/game')
+def game():
+    return render_template("index.html")
+
+@app.route('/')
+def home():
+    return "<a href='/game'>Переход...</a>"
+
+if __name__ == '__main__':
+    app.run()
