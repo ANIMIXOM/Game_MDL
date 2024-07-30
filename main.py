d2327544173e735fe638fdb989c03045c675f0e6
ANIMIXOM <maximshlyamnev@yandex.ru>
2024-07-30 19:50:24 +0300

    second_game

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/game')
-def game():
-    return render_template("index.html")
+
+@app.route('/game/<index>')
+def game(index):
+    return render_template(f"index_{index}.html")
+
 
 @app.route('/')
 def home():
-    return "<a href='/game'>Переход...</a>"
+    return """<a href='/game/1'>LTZ</a>
+    <a href='/game/3'>KABEL</a>"""
+
 
 @app.route("/end")
 def end():
     return """<a href='/'>Повторить?</a>"""
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
